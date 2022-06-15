e018b334ab3302fb0e8dbb10c737bc61528e71e9
MuhammadZakyR <muhammadzaky.19058@mhs.unesa.ac.id>
2022-06-16 03:56:37 +0700

    First Commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    container: {
+      center: true,
+      padding: '16px',
+    },
+    extend: {
+      colors: {
+        primary: '#047857',
+        sekunder: '#a7f3d0',
+        hprimary: '#065f46',
+        hsekunder: '#6ee7b7',
+        teks: '#0f172a',
+        hteks: '#cbd5e1',
+      },
+    },
+  },
+  plugins: [],
+}
